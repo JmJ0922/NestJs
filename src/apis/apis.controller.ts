a37dbb51647447b4c5f55d2daf5d9c2df7f52c41
JmJ0922 <jainmariajustine@gmail.com>
2024-03-09 15:52:08 +0530

    controller

--- src/apis/apis.controller.ts
+++ src/apis/apis.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller,Delete,Get,Param,Post, Put, Query } from '@nestjs/common';
+import { CreateApiDto } from './dto/create-api.dto';
+import { UpdateApiDto } from './dto/update-api.dto';
 
 @Controller('apis')
-export class ApisController {}
+export class ApisController {
+
+//GET /apis?type=fast --> []
+@Get()
+getApis(@Query('type') type: string){
+    return [{ type }];
+}
+//GET /apis/:id --> {...}
+@Get(':id')
+getOneApi(@Param('id') id: string){
+    return {
+        id,
+    };
+}
+//POST /apis
+@Post()
+createApi(@Body() createApiDta: CreateApiDto){
+    return {
+        name: createApiDta.name,
+    };
+}
+//PUT /apis/:id --> {...}
+@Put(':id')
+updateApi(@Param('id') id: string, @Body() UpdateApiDto: UpdateApiDto){
+    return {
+        id,
+        name:UpdateApiDto,
+    };
+}
+//DELETE /apis/:id
+@Delete(':id')
+removeApi(@Param('id') id: string){
+    return {};
+}
+
+}
---
+++ src/apis/dto/create-api.dto.ts
@@ -0,0 +1,3 @@
+export class CreateApiDto {
+    name:String
+}
